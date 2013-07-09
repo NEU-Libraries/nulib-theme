4aefcf8483e6f15bb76d6bc79e068d97e7f67373
Steven Bassett <steven.j.bassett@gmail.com>
2013-07-09 11:43:49 -0400

    Adding the starter style guide

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
       },
   });
 
-
+  
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-jekyll');
