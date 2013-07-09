dfa25719fc6271a608be35dfa5b37d050bbed0ed
Steven Bassett <steven.j.bassett@gmail.com>
2013-07-09 10:29:12 -0400

    Adding the raw style docs using jekyll.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {                  // Task
+        dist: {                   // Target
+          options: {              // Target options
+            sassDir: 'scss',
+            cssDir: 'css',
+            environment: 'production'
+          }
+        },
+        development: {
+          options: {
+            sassDir: 'scss',
+            cssDir: 'css',
+            environment: 'development'
+          }
+        }
+        docs:{
+          options: {
+            sassDir: 'scss',
+            cssDir: 'docs/assets/css',
+            environment: 'development' 
+          }
+        },
+      },
+  });
+
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jekyll');
+};
